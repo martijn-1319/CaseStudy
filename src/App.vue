f6d0bdf6ff72e8e36380c603122524edfcfdba65
Jelle <jellehak@gmail.com>
2022-05-24 10:48:04 +0200

    add Title component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <v-app>
     <v-main>
-      <v-container>Hello world</v-container>
+      <v-container>
+          <Title>Overzicht van je rewards</Title>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus.
+      </v-container>
     </v-main>
   </v-app>
 </template>
---
+++ src/components/Title.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="Title">
+    <slot />
+  </div>
+</template>
+
+<style>
+.Title {
+  text-align: left;
+  font: normal normal 800 32px/38px Open Sans;
+  letter-spacing: 0px;
+  color: #087164;
+  opacity: 1;
+}
+</style>
